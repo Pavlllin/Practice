250347ebc59d80076cb29355a020d1ca2336030b
Mediasoft <pavlin.svz@gmail.com>
2021-03-10 14:53:29 +0400

    Правки моделей и добавление вьюх

--- notes/models.py
+++ notes/models.py
@@ -3,16 +3,42 @@ from django.db import models
 
 # Create your models here.
 class Note(models.Model):
-    text = models.CharField(max_length=500,
+    class Meta:
+        verbose_name = "Записи"
+        verbose_name_plural = "Записи"
+        db_table = "note"
+
+    text = models.CharField(max_length=5000,
                             verbose_name="Текст записи")
     author = models.ForeignKey('notes.User', on_delete=models.CASCADE,
                                verbose_name="Автор записи")
+    address = models.ForeignKey('notes.Address', on_delete=models.CASCADE,
+                                 verbose_name="адрес записи", null=True)
+
+
+class Address(models.Model):
+    class Meta:
+        verbose_name = "Ссылки"
+        verbose_name_plural = "Ссылки"
+        db_table = "address"
+
+    slug_url = models.SlugField(null=True)
 
 
 class User(models.Model):
+    class Meta:
+        verbose_name = "Пользователи"
+        verbose_name_plural = "Пользователи"
+        db_table = "user"
+
     name = models.CharField(max_length=100,
                             verbose_name="Имя пользователя")
     login = models.CharField(max_length=100,
                              verbose_name="Логин")
-    password = models.CharField(max_length=100,
+    password = models.CharField(max_length=1000,
                                 verbose_name="Пароль")
+
+# модель(слаг)+
+# Длина +
+# Мета(verbose + plural, table_name) +
+# ссылки в нотах +
--- notes/views.py
+++ notes/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from rest_framework import generics
+
+from .models import User, Note
+from .serializers import UserSerializer, NoteSerializer
+
 
 # Create your views here.
+
+# class UserListView(generics.ListAPIView, generics.CreateAPIView):
+#     queryset = User.objects.all()
+#     serializer_class = UserSerializer
+
+#Работа с моделью (удаление, пут, создание, получение)
+
+class NoteListView(generics.ListAPIView, generics.CreateAPIView):
+    queryset = Note.objects.all()
+    serializer_class = NoteSerializer
