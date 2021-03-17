a5698df0f9a422f19541d1b5914ee06eb1bf0f9d
Mediasoft <pavlin.svz@gmail.com>
2021-03-17 12:19:26 +0400

    Скачивание записок конкретного пользователя

--- notes/urls.py
+++ notes/urls.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
 from django.urls import path
-from .views import NoteListView,NoteView
+from .views import NoteListView,NoteView,NoteDownloadView
 
 urlpatterns = [
     path('notes/', NoteListView.as_view()),
+    path('notes/download/', NoteDownloadView.as_view()),
     path('notes/<slug:slug_url>/', NoteView.as_view()),
 ]
--- users/admin.py
+++ users/admin.py
@@ -1,4 +1,19 @@
 from django.contrib import admin
 from .models import User
 # Register your models here.
-admin.site.register(User)
+
+
+
+def note_report(queryset):
+
+    queryset.update(status='p')
+
+note_report.short_description = "Отчет по всем записям пользователей"
+
+class ArticleAdmin(admin.ModelAdmin):
+    list_display = ['login', 'name']
+    ordering = ['login']
+    actions = [note_report]
+
+admin.site.register(User, ArticleAdmin)
+
