29062b9649405b0182752184df0fd45601d55c99
Mediasoft <pavlin.svz@gmail.com>
2021-03-19 16:33:20 +0400

    Finally celery

--- pastebin/celery.py
+++ pastebin/celery.py
@@ -1,10 +1,20 @@
 import os
 from celery import Celery
+from celery.schedules import crontab
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'pastebin.settings')
 
 app = Celery('pastebin')
 app.config_from_object('django.conf:settings', namespace='CELERY')
+
+app.conf.beat_schedule = {
+    # Executes every Monday morning at 7:30 a.m.
+    'add-every-monday-morning': {
+        'task': 'users.tasks.create_report',
+        'schedule': crontab(hour=12, minute=31),
+    },
+}
+
 
 # Load task modules from all registered Django app configs.
 app.autodiscover_tasks()
--- users/tasks.py
+++ users/tasks.py
@@ -4,12 +4,15 @@ from .models import User
 import csv
 
 @shared_task()
-def create_report(user_ids):
+def create_report(user_ids = None):
     with open('content/report' + str(datetime.now().strftime("%m_%d_%Y_%H_%M_%S")) + ".csv", 'w', newline='',
               encoding='utf-8') as file:
         csv_file = csv.writer(file, delimiter=',')
         csv_file.writerow(["Логин", "Текст записи", "Адрес записи", "Тип записи"])
-        queryset = User.objects.filter(pk__in=user_ids)
+        if(user_ids is None):
+            queryset = User.objects.all()
+        else:
+            queryset = User.objects.filter(pk__in=user_ids)
         queryset = queryset.prefetch_related("note_author__type_of_text").values('note_author__text',
                                                                                  'note_author__slug_address',
                                                                                  'note_author__type_of_text', 'login')
